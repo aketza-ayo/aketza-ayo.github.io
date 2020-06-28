4b2176d3dae8153170ca07685beb099ec4818f25
Aketza Ayo <aketzamaster@gmail.com>
2020-06-28 15:31:14 +0200

    Various changes

--- dist/script.js
+++
@@ -1,2 +0,0 @@
-let litoffTime = new Date("7 December 2019 9:56:00 GMT+01:00");
-$("#defaultCountdown").countdown({until: litoffTime, format: 'HMS'});
---
+++ js/script.js
@@ -0,0 +1,2 @@
+let litoffTime = new Date("31 October 2020 10:00:00 GMT+01:00");
+$("#defaultCountdown").countdown({until: litoffTime, format: 'DHMS'});
